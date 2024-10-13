f3d4b3785d7aa75e4b5363b6563d208a1c5f6c5b
JavokhirMuminov77 <javokhirmuminovdev@gmail.com>
2024-10-14 07:12:44 +0900

    feat: create project standart

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import  express  from "express";
 import path from "path";
 import router from "./router";
 import routerAdmin from "./routerAdmion";
+import morgan from "morgan";
+import { MORGAN_FORMAT } from "./libs/config";
 
 
 
@@ -12,6 +14,7 @@ const app = express();
 app.use(express.static(path.join( __dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(morgan(MORGAN_FORMAT));
 
 //2 Sessions 
 
---
+++ src/libs/config.ts
@@ -0,0 +1,4 @@
+export const MORGAN_FORMAT = `:method :url :response-time [:status] \n`;
+
+
+
