fcf3ddef148446392b82a4dccdebdbd695fb89a8
JavokhirMuminov77 <javokhirmuminovdev@gmail.com>
2024-10-14 23:34:07 +0900

    fix: modefy Admin login and signup procces

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -2,6 +2,7 @@ import MemberModel from "../schema/Member.model";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { MemberType } from "../libs/enums/member.enum";
+import * as bcrytp from "bcryptjs";
 
 class MemberService {
     private readonly memberModel;
@@ -9,6 +10,7 @@ class MemberService {
         this.memberModel = MemberModel;
     }
 
+    //ProcessLogin
     public async processLogin (input: LoginInput): Promise <MemberType> {
         const member = await this.memberModel
         .findOne({memberNick: input.memberNick},
@@ -16,28 +18,27 @@ class MemberService {
         .exec();
         if(!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
         const isMatch  = input.memberPassword === member.memberPassword;
-        console.log("isMatch", isMatch);
+  
         if (!isMatch) {
             throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
         }
-        console.log("member", member);
-        return member;
+        const result = await this.memberModel.findById(member._id).exec();
+      
+        return result;
     }
 
 
-
-
-
-
-
-
+    //ProcessSignup
     public async processSignup(input: MemberInput): Promise <Member> {
         const exist = await this.memberModel
         .findOne({memberType: MemberType.ADMIN})
         .exec();
 
         if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+    
 
+        const salt = await  bcrytp.genSalt();
+        input.memberPassword = await bcrytp.hash(input.memberPassword, salt);
         try {
             const tempResult  = new this.memberModel(input);
             const result = await tempResult.save(); 
